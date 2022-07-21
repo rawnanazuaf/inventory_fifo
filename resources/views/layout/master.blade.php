1de69a0738f6110241ea0229bd9edcc0366b10a9
rawnanazuaf <rawnanazuafdammahum@gmail.com>
2022-07-21 09:20:05 +0700

    user

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -68,7 +68,7 @@
           <img src="AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Admin</a>
+          <a href="#" class="d-block">{{auth()->user()->name}}</a>
         </div>
       </div>
 
