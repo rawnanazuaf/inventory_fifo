6ff80ea3b74af909baa3bccdbdf4f1a48939b594
muhammadfauzananwar <muhammadfauzananwar@gmail.com>
2022-12-16 14:18:00 +0700

    wkwkwk

--- resources/views/ledger/index.blade.php
+++ resources/views/ledger/index.blade.php
@@ -6,12 +6,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>DATA PRODUK</h1>
+                <h1>DATA LEDGER</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="/index">MENU</a></li>
-                    <li class="breadcrumb-item active">DATA PRODUK</li>
+                    <li class="breadcrumb-item active">DATA LEDGER</li>
                 </ol>
             </div>
         </div>
@@ -25,7 +25,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Table Data Produk</h3>
+                        <h3 class="card-title">Table Data Ledger</h3>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
